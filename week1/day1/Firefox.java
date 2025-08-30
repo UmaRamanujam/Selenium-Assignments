8c8e8be38a45383aae63aac097d6b7e8e1924f42
UmaRamanujam <arv.uma@gmail.com>
2025-08-30 00:30:18 -0400

    Add files via upload
    Week1 Assignments

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+	    int releaseYear = 1998;
+	    char browserLogo = 'f';
+	    System.out.println("Let's see about firefox in Java DataTypes Assignements." +"\n" +"The browerversion of firefox is " +browserVersion +"\n" +"Browser Name is " +browserName +"\n" +"The Visibility is " +isVisible +"\n" +"The Release Year is " +releaseYear +"\n" +"The browser Logo is " +browserLogo);
+	    		
+				
+
+	}
+
+}
---
+++ week1/day2/EdgeBrowser.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class EdgeBrowser {
+	
+
+public static void main(String[] args) 
+	{
+		Browser b=new Browser();
+		b.launchBrowser("Edge");
+		b.loadUrl();
+		System.out.println("It is an Edge Browser.");
+	}
+	
+	
+}
